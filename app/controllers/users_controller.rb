f6b2fd90b5f6d4cd5b116c66a700f2eddb719a09
Dennis Ojeda <royojeda1@gmail.com>
2022-07-26 15:55:27 +0800

    Update Post card styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
     @profile = @user.profile
     @friend_requests = @user.friend_requests
     @other_users = User.where.not(id: @user.id) # .order("random()")
+    # @posts = Post.where(author: current_user)
+    @posts = Post.all.order('posts.created_at DESC')
   end
 end
