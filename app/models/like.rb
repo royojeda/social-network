888fe5a3107b9dda53b3ea3a143cc50029d4bb1f
Dennis Ojeda <royojeda1@gmail.com>
2022-07-28 06:50:23 +0800

    Add Like validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
+  validates :liker_id, uniqueness: { scope: [:likeable_id, :likeable_type] }
+
   belongs_to :liker, class_name: "User"
   belongs_to :likeable, polymorphic: true
 end
