93d5b133a923de70db19477b5d77760023eb6df4
Dennis Ojeda <royojeda1@gmail.com>
2022-07-27 14:42:46 +0800

    Add turbo-frame for liking and unliking posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,19 +2,15 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(like_params)
 
-    if @like.save
-      redirect_to user_path(current_user)
-    else
-      render "users/show"
-    end
+    @like.save
+
+    render "users/show" unless @like.save
   end
 
   def destroy
     @like = Like.find_by(like_params)
 
     @like.destroy
-
-    redirect_to user_path(current_user)
   end
 
   private
