4bff89088f4bac690715cf2c07149d62786bac36
Dennis Ojeda <royojeda1@gmail.com>
2022-07-29 05:31:02 +0800

    Update posts#create to respond with turbo stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,15 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
 
-    if @post.save
-      redirect_to posts_path
-    else
-      render "users/show"
+    respond_to do |format|
+      if @post.save
+        # redirect_to posts_path
+        format.turbo_stream
+      else
+        format.html do
+          redirect_to authenticated_root_path
+        end
+      end
     end
   end
 
