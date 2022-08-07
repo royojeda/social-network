4185a2de8756eb8cd857c9eb3f604400b60b0cfa
Dennis Ojeda <royojeda1@gmail.com>
2022-08-08 02:51:05 +0800

    Update comment button action
    Clears comment textarea value.
    
    Focuses comment textarea.

--- app/javascript/controllers/comment_form_controller.js
+++ app/javascript/controllers/comment_form_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "form" ]
+  static targets = [ "form", "textArea" ]
 
   clicked() {
+    this.textAreaTarget.value = ""
     this.formTarget.classList.toggle("hidden")
+    this.textAreaTarget.focus()
   }
 }
