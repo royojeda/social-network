d1735453810e30bf8692a0aa046af3446bd6a225
Dennis Ojeda <royojeda1@gmail.com>
2022-08-12 22:24:55 +0800

    Add gravatar profile photo

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,15 @@
 class Profile < ApplicationRecord
+  require 'digest'
+
   belongs_to :user
+
+  def gravatar_photo_url
+    "https://www.gravatar.com/avatar/#{md5_hash}?d=mp"
+  end
+
+  private
+
+  def md5_hash
+    Digest::MD5.hexdigest user.email.strip.downcase
+  end
 end
