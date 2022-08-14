42ad5b555b33bb7a4f99e2c5be748d1cc274da54
Dennis Ojeda <royojeda1@gmail.com>
2022-08-15 00:46:07 +0800

    Fix bug with comment button
    Comment button wasn't working because of nested stimulus controllers.

--- app/javascript/controllers/comments_visibility_controller.js
+++ app/javascript/controllers/comments_visibility_controller.js
@@ -1,9 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "comments" ]
+  static targets = [ "comments", "form", "textArea" ]
 
   toggle() {
     this.commentsTarget.classList.toggle("hidden")
+  }
+
+  toggleForm() {
+    this.textAreaTarget.value = ""
+    this.formTarget.classList.toggle("hidden")
+    this.textAreaTarget.focus()
   }
 }
